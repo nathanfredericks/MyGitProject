e4081882ab98e68298d390e0802d1cb6ac4ec87f
Nathan Fredericks <nathan@fredericks.dev>
2025-01-27 14:55:20 -0400

    Add iterative version of Fibonacci sequence

--- math.py
+++ math.py
@@ -7,3 +7,17 @@ def recursiveFibonacci(int num):
 		return 1
 	#recursively calls itself until it gets to num == 1 and num == 0
 	return (recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2))
+
+def iterativeFibonacci(num):
+    if num == 0 or num == 1:
+        return 1
+
+    fib_0 = 1
+    fib_1 = 1
+
+    for _ in range(2, num + 1):
+        fib_next = fib_0 + fib_1
+        fib_0 = fib_1
+        fib_1 = fib_next
+
+    return fib_1
