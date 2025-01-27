b82dc3676593268266087cf098a0805252eb37eb
Nathan Fredericks <nathan@fredericks.dev>
2025-01-27 15:05:40 -0400

    Add comments to code

--- fib.py
+++ fib.py
@@ -9,12 +9,15 @@ def recursiveFibonacci(num):
 	return (recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2))
 
 def iterativeFibonacci(num):
+    # If the requested number is 0 or 1, return 1
     if num == 0 or num == 1:
         return 1
 
+    # Initialize the first two Fibonacci numbers
     fib_0 = 1
     fib_1 = 1
 
+    # Calculate Fibonacci iteratively up to the desired number
     for _ in range(2, num + 1):
         fib_next = fib_0 + fib_1
         fib_0 = fib_1
